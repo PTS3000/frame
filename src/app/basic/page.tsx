41750ae29387d057184077b305b9dba9dd0309fb
luisparrajunitec <luisjgcsp@gmail.com>
2024-06-20 13:47:22 +0100

    modified splashscreen

--- src/app/basic/page.tsx
+++ src/app/basic/page.tsx
@@ -6,11 +6,11 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Basic Frame',
     description: 'A Farcaster Frame Demo',
-    images: [`${process.env.NEXT_PUBLIC_SITE_URL}/site-preview.png`],
+    images: [`${process.env.NEXT_PUBLIC_SITE_URL}/site-preview.jpg`],
   },
   other: {
     'fc:frame': 'vNext',
-    'fc:frame:image': `${process.env.NEXT_PUBLIC_SITE_URL}/site-preview.png`,
+    'fc:frame:image': `${process.env.NEXT_PUBLIC_SITE_URL}/site-preview.jpg`,
     'fc:frame:post_url': `${process.env.NEXT_PUBLIC_SITE_URL}/api/basic?id=1`,
     'fc:frame:button:1': 'Start',
   },
